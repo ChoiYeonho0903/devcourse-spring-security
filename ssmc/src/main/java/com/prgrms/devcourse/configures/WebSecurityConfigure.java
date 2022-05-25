ceae7382fec25e0fe1cfad357c744630047c6249
ChoiYeonho0903 <903yh@naver.com>
2022-05-25 15:54:39 +0900

    프로젝트 생성

---
+++ ssmc/src/main/java/com/prgrms/devcourse/configures/WebSecurityConfigure.java
@@ -0,0 +1,83 @@
+package com.prgrms.devcourse.configures;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/asserts/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+            .antMatchers("/me").hasAnyRole("USER", "ADMIN")
+            .antMatchers("/admin").access("hasRole('ADMIN') and isFullyAuthenticated()")
+            .anyRequest().permitAll()
+            .and()
+            .formLogin()
+            .defaultSuccessUrl("/")
+            .permitAll();
+        http.logout()
+            .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+            .logoutSuccessUrl("/")
+            .invalidateHttpSession(true)
+            .clearAuthentication(true);
+        http.rememberMe()
+            .rememberMeParameter("remember-me")
+            .tokenValiditySeconds(300);
+//        http.requiresChannel()
+//            .antMatchers("/api/**").requiresSecure();
+        http.anonymous()
+            .principal("thisIsAnonymousUser")
+            .authorities("ROLE_ANONYMOUS", "ROLE_UNKNOWN");
+        http.exceptionHandling()
+            .accessDeniedHandler(accessDeniedHandler());
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("user").password("{noop}user123").roles("USER")
+            .and()
+            .withUser("admin").password("{noop}admin123").roles("ADMIN")
+            ;
+    }
+
+    public AccessDeniedHandler accessDeniedHandler() {
+        return (request, response, e) -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Object principal = authentication != null ? authentication.getPrincipal() : null;
+            log.warn("{} is denied", principal, e);
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.setContentType("text/plain");
+            response.getWriter().write("## ACCESS DENIED ##");
+            response.getWriter().flush();
+            response.getWriter().close();
+        };
+
+    }
+}
